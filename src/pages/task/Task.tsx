82f9453c4ac2f7f50707191b363c07c32a4c36d0
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2023-12-07 15:25:46 +0900

    [modify] タスク一覧のデザインを調整

--- src/pages/task/Task.tsx
+++ src/pages/task/Task.tsx
@@ -1,32 +1,38 @@
 import Stepper from "../../components/stepper/Stepper";
 import style from "./Task.module.scss";
 
+import IconPin from "@mui/icons-material/PushPin";
+
 const Task = () => {
     return (
         <div className={style.container}>
             <div className={style.task}>
-                <span className={style.username}>User01</span>
-                <div className={style.buyAt}>
+                <div className={`${style.infomation} ${style.isPinned}`}>
+                    <IconPin />
+                </div>
+                <div className={style.user}>
+                    <span>RilyHugu</span>
+                </div>
+                <div className={`${style.infomation} ${style.buyAt}`}>
                     <span className={style.label}>購入日</span>
                     <span>12/1 (2日経過)</span>
                 </div>
-                <div className={style.deliveryAt}>
+                <div className={`${style.infomation} ${style.deliveryAt}`}>
                     <span className={style.label}>納品予定日</span>
                     <span>12/15 (残り14日)</span>
                 </div>
-                <div className={style.sales}>
+                <div className={`${style.infomation} ${style.sales}`}>
                     <span className={style.label}>売上</span>
                     <span>3,000円 (2,340円)</span>
                 </div>
-                <div className={style.amount}>
+                <div className={`${style.infomation} ${style.amount}`}>
                     <span className={style.label}>納入枚数</span>
                     <span>1点</span>
                 </div>
-                <span className={style.status}>
-                    <span className={style.label}>納入枚数</span>
-                </span>
+                <div className={style.progress}>
+                    <Stepper steps={["確認", "ラフ", "清書", "納品"]} active={1} />
+                </div>
             </div>
-            <Stepper steps={["確認", "ラフ", "清書", "納品"]} active={1} />
         </div>
     );
 }
