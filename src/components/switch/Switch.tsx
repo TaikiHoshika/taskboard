ced76c3673ae963a9f0ea3ee99c6b5259da96f35
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2023-12-18 14:39:36 +0900

    [modify] タスク登録フォームのデザインを変更

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -1,13 +1,22 @@
+import { ChangeEvent } from "react";
 import style from "./Switch.module.scss";
 
 type props = {
-    onChange: () => void;
+    onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+    name?: string;
 }
 
 const Switch = (props: props) => {
     return (
         <div>
-            <input type="checkbox" onChange={props.onChange} className={style.switch} />
+            <input
+                id={props.name}
+                type="checkbox"
+                name={props.name}
+                onChange={props.onChange}
+                className={style.switch}
+            />
+            <label htmlFor={props.name}></label>
         </div>
     );
 }
--- src/pages/task/components/CreateTask.tsx
+++ src/pages/task/components/CreateTask.tsx
@@ -4,13 +4,7 @@ import style from "./CreateTask.module.scss";
 import IconClose from "@mui/icons-material/Close";
 import axios from "axios";
 import Switch from "../../../components/switch/Switch";
-import { useState } from "react";
-
-const options = [
-    {value: "10000", label: "User01"},
-    {value: "10001", label: "User02"},
-    {value: "10002", label: "User03"},
-]
+import { ChangeEvent, FormEvent } from "react";
 
 type props = {
     isShow: boolean;
@@ -18,9 +12,6 @@ type props = {
 }
 
 const CreateTask = (props: props) => {
-    const [isCreateNewUser, setIsCreateNewUser] = useState<boolean>(false);
-    const [isPinned, setIsPinned] = useState<boolean>(false);
-
     const onCreate = () => {
         axios.post("http://localhost:8000/task/create", {
             test: "test"
@@ -31,74 +22,73 @@ const CreateTask = (props: props) => {
         })
     }
 
-    const onChangeIsCreateNewUser = () => {
-        setIsCreateNewUser((prev) => !prev);
-    }
-    const onChangeIsPinned = () => {
-        setIsPinned((prev) => !prev);
+    const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = new FormData(event.currentTarget);
+        console.log(`createNewUser?: ${form.get("isCreateNewUser")}`);
     }
 
     return (props.isShow ? (
         <Modal>
             <div className={style.container}>
-                <form>
+                <form onSubmit={handleOnSubmit}>
                     <div className={style.title}>
                         <span>タスクを登録</span>
                     </div>
                     <div className={style.switch}>
-                        <Switch onChange={onChangeIsCreateNewUser} />
-                        <span>新規ユーザーを登録 {isCreateNewUser + ""}</span>
+                        <Switch name={"isExistUser"} />
+                        <span>既存ユーザーを選択</span>
                     </div>
                     <div className={style.switch}>
-                        <Switch onChange={() => alert("test")} />
-                        <span>ピン止め {isPinned + ""}</span>
+                        <Switch name={"isPinned"} />
+                        <span>ブックマークに追加</span>
                     </div>
                     <div className={style.row}>
                         <div>
                             <span>ユーザー名</span>
-                            <select disabled={isCreateNewUser}>
+                            <input type="text" name="userName" />
+                        </div>
+                        <div>
+                            <span>ユーザー名</span>
+                            <select>
                                 <option>User01</option>
                                 <option>User02</option>
                                 <option>User03</option>
                             </select>
                         </div>
-                        <div>
-                            <span>ユーザー名</span>
-                            <input type="text" disabled={!isCreateNewUser} />
-                        </div>
                     </div>
                     <div className={style.row}>
                         <div>
+                            <span>ユーザーID</span>
+                            <input type="number" name="userId" />
                         </div>
                         <div>
-                            <span>ユーザーID</span>
-                            <input type="number" disabled={!isCreateNewUser} />
                         </div>
                     </div>
                     <div className={style.row}>
                         <div>
                             <span>トークルームURL</span>
-                            <input type="text" />
+                            <input type="text" name="talkroomId" />
                         </div>
                     </div>
                     <div className={style.row}>
                         <div>
                             <span>購入日</span>
-                            <input type="date" />
+                            <input type="date" name="buyAt" />
                         </div>
                         <div>
                             <span>納品予定日</span>
-                            <input type="date" />
+                            <input type="date" name="delivaryAt" />
                         </div>
                     </div>
                     <div className={style.row}>
                         <div>
                             <span>納入枚数</span>
-                            <input type="number" />
+                            <input type="number" name="amount" />
                         </div>
                         <div>
                             <span>売上</span>
-                            <input type="number" />
+                            <input type="number" name="sales" />
                         </div>
                     </div>
                     <div className={style.submit}>
