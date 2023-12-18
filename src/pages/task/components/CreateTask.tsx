ffbabac83007e037beb2f5df4cff6a9091895193
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2023-12-18 16:20:57 +0900

    [modify] フォームとスイッチの紐づけ

--- src/pages/task/components/CreateTask.tsx
+++ src/pages/task/components/CreateTask.tsx
@@ -4,7 +4,7 @@ import style from "./CreateTask.module.scss";
 import IconClose from "@mui/icons-material/Close";
 import axios from "axios";
 import Switch from "../../../components/switch/Switch";
-import { ChangeEvent, FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 type props = {
     isShow: boolean;
@@ -12,21 +12,32 @@ type props = {
 }
 
 const CreateTask = (props: props) => {
-    const onCreate = () => {
+
+    const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = new FormData(event.currentTarget);
+        if(selectExistUser) {
+            console.log(form.get("user"));
+        } else {
+            console.log(form.get("userName"));
+            console.log(form.get("userId"));
+        }
+        console.log(form.get("talkroomUrl"));
+        console.log(form.get("buyAt"));
+        console.log(form.get("delivaryAt"));
+        console.log(form.get("amount"));
+        console.log(form.get("sales"));
+
         axios.post("http://localhost:8000/task/create", {
             test: "test"
         }).then((response) => {
             console.log(response.data);
         }).catch(() => {
-            console.log("Error");
+            console.log("APIに接続できませんでした");
         })
     }
 
-    const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const form = new FormData(event.currentTarget);
-        console.log(`createNewUser?: ${form.get("isCreateNewUser")}`);
-    }
+    const [selectExistUser, setSelectExistUser] = useState<boolean>(false);
 
     return (props.isShow ? (
         <Modal>
@@ -36,21 +47,24 @@ const CreateTask = (props: props) => {
                         <span>タスクを登録</span>
                     </div>
                     <div className={style.switch}>
-                        <Switch name={"isExistUser"} />
+                        <Switch
+                            name={"selectExistUser"}
+                            onChange={() => setSelectExistUser((prev) => (!prev))}
+                        />
                         <span>既存ユーザーを選択</span>
                     </div>
                     <div className={style.switch}>
-                        <Switch name={"isPinned"} />
+                        <Switch name={"addPinned"} />
                         <span>ブックマークに追加</span>
                     </div>
                     <div className={style.row}>
                         <div>
                             <span>ユーザー名</span>
-                            <input type="text" name="userName" />
+                            <input type="text" name="userName" disabled={selectExistUser} />
                         </div>
                         <div>
                             <span>ユーザー名</span>
-                            <select>
+                            <select name="user" disabled={!selectExistUser}>
                                 <option>User01</option>
                                 <option>User02</option>
                                 <option>User03</option>
@@ -60,7 +74,7 @@ const CreateTask = (props: props) => {
                     <div className={style.row}>
                         <div>
                             <span>ユーザーID</span>
-                            <input type="number" name="userId" />
+                            <input type="number" name="userId" disabled={selectExistUser} />
                         </div>
                         <div>
                         </div>
@@ -68,7 +82,7 @@ const CreateTask = (props: props) => {
                     <div className={style.row}>
                         <div>
                             <span>トークルームURL</span>
-                            <input type="text" name="talkroomId" />
+                            <input type="text" name="talkroomUrl" />
                         </div>
                     </div>
                     <div className={style.row}>
@@ -84,11 +98,11 @@ const CreateTask = (props: props) => {
                     <div className={style.row}>
                         <div>
                             <span>納入枚数</span>
-                            <input type="number" name="amount" />
+                            <input type="number" name="amount" defaultValue={1} />
                         </div>
                         <div>
                             <span>売上</span>
-                            <input type="number" name="sales" />
+                            <input type="number" name="sales" defaultValue={3000} />
                         </div>
                     </div>
                     <div className={style.submit}>
